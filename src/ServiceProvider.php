2b33176711aff35578bc999a2799c420177d57a8
Vioserver <vioserver7@gmail.com>
2023-11-07 16:36:43 +0330

    update service provider main project

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -40,6 +40,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
      */
     public function boot()
     {
+		$this->publishes([__DIR__ . '/config/config.php' => config_path('hivelink.php')],'config');
         return $this->provider->boot();
     }
 
